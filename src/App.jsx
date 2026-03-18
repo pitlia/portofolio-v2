07816875a015cb11e01d2b06f7dd1690ec0011e8
pitlia <fi4040308@gmail.com>
2026-03-18 09:26:51 +0700

    feat: add statistics section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Stats from './components/Stats';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       
       <main>
         <Hero />
+        <Stats />
         {/* Sections will go here */}
       </main>
 
